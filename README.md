5c19695116e5316376d98575af3a0e028a668d1b
Barash Sharma <138241217+barash1311@users.noreply.github.com>
2026-04-30 22:40:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# job-tracker-backend
+JOB tracker api
---
+++ src/test/java/com/barash/jobtrackerbackend/JobTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barash.jobtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
